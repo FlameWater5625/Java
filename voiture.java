afccef9719198ce38e500deea3e4fbdb6a93c326
FlameWater5625 <minguillonerwan@gmail.com>
2025-02-05 14:57:20 +0100

    voiture

---
+++ voiture.java
@@ -0,0 +1,17 @@
+// creer une classe voiture
+public class voiture {
+    // attributs
+    public String couleur;
+    public String marque;
+    public int annee;
+    // methodes
+    public void demarrer() {
+        System.out.println("La voiture demarre");
+    }
+    public void accelerer() {
+        System.out.println("La voiture accelere");
+    }
+    public void arreter() {
+        System.out.println("La voiture arrete");
+    }
+}
