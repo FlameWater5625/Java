5d51e4fcd3a220c831d20c2a12f39496780549da
FlameWater5625 <minguillonerwan@gmail.com>
2025-03-04 12:37:43 +0100

    xcxcxcxcx

---
+++ sql/src/main/java/com/monprojet/Utilisateur.java
@@ -0,0 +1,37 @@
+package com.monprojet;
+
+public class Utilisateur {
+    private int id;
+    private String nom;
+    private String email;
+
+    public Utilisateur(int id, String nom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void __String() {
+        System.out.println("Nom: " + nom + ", Email: " + email);
+    }
+}
